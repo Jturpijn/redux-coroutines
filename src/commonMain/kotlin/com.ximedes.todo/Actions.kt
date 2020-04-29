f2c34847a42cb6c213a57ee8f251528c36de2cda
Joris Portegies Zwart <joris.portegies.zwart@ximedes.com>
2020-04-29 10:11:55 +0200

    Make serialization work

--- src/commonMain/kotlin/com.ximedes.todo/Actions.kt
+++ src/commonMain/kotlin/com.ximedes.todo/Actions.kt
@@ -1,17 +1,20 @@
 package com.ximedes.todo
 
+import kotlinx.serialization.Serializable
+
 /*
  * Actions for to-do example
  */
 sealed class Action()
-class AddTodo(val text: String): Action()
-object Increment: Action()
-class ToggleTodo(val index: Int): Action()
-class SetVisibilityFilter(val filter: VisibilityFilter): Action()
+class AddTodo(val text: String) : Action()
+object Increment : Action()
+class ToggleTodo(val index: Int) : Action()
+class SetVisibilityFilter(val filter: VisibilityFilter) : Action()
 
 /*
  * More data such as the custom To-do class and VisibilityFilter
  */
+@Serializable
 data class Todo(
     val text: String,
     val completed: Boolean = false,
--- src/jsMain/kotlin/com.ximedes.todo/Index.kt
+++ src/jsMain/kotlin/com.ximedes.todo/Index.kt
@@ -4,15 +4,17 @@ import com.ximedes.redux.ReducerStore
 import com.ximedes.redux.SagaContainer
 import com.ximedes.redux.applyMiddleware
 import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
-import io.ktor.client.features.*
 import kotlinx.coroutines.delay
-import kotlinx.io.core.use
 
 val container = SagaContainer<State, Action>()
 val mStore = applyMiddleware(ReducerStore(todoReducer, State()), container.createMiddleWare())
 val client = HttpClient() {
-//    install(JsonFeature)
+    install(JsonFeature) {
+        serializer = KotlinxSerializer()
+    }
 }
 
 suspend fun main() {
@@ -20,9 +22,7 @@ suspend fun main() {
     mStore.dispatch(AddTodo("Finish POC"))
     var todos = listOf<Todo>()
     container.runSaga {
-        val response = HttpClient().use {
-            it.get<List<Todo>>("http://127.0.0.1:8080/getTodos")
-        }
+        val response = client.get<List<Todo>>("http://127.0.0.1:8080/getTodos")
         println(response)
     }
     println("Retrieving off site todos : ${todos}")
